aafbcf763cf8d9b3ba9c5aeee5c3678eb31bef8f
Nguyen Quy Minh <nqminh1993@gmail.com>
2025-01-09 14:40:52 +0700

    upgrade sdk version

--- resources/views/join-meeting.blade.php
+++ resources/views/join-meeting.blade.php
@@ -4,8 +4,8 @@
     <title>Join Zoom Meeting</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/2.9.5/css/bootstrap.css" />
-    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/2.9.5/css/react-select.css" />
+    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/3.11.0/css/bootstrap.css" />
+    <link type="text/css" rel="stylesheet" href="https://source.zoom.us/3.11.0/css/react-select.css" />
 </head>
 <body class="ReactModal__Body--open">
     <div id="zmmtg-root"></div>
@@ -17,18 +17,18 @@
     <div class="global-pop-up-box"></div>
     <div class="sharer-controlbar-container sharer-controlbar-container--hidden"></div>
 
-    <script src="https://source.zoom.us/2.9.5/lib/vendor/react.min.js"></script>
-    <script src="https://source.zoom.us/2.9.5/lib/vendor/react-dom.min.js"></script>
-    <script src="https://source.zoom.us/2.9.5/lib/vendor/redux.min.js"></script>
-    <script src="https://source.zoom.us/2.9.5/lib/vendor/redux-thunk.min.js"></script>
-    <script src="https://source.zoom.us/2.9.5/lib/vendor/lodash.min.js"></script>
-    <script src="https://source.zoom.us/zoom-meeting-2.9.5.min.js"></script>
-    <script src="https://source.zoom.us/2.9.5/zoom-meeting-embedded-2.9.5.min.js"></script>
+    <script src="https://source.zoom.us/3.11.0/lib/vendor/react.min.js"></script>
+    <script src="https://source.zoom.us/3.11.0/lib/vendor/react-dom.min.js"></script>
+    <script src="https://source.zoom.us/3.11.0/lib/vendor/redux.min.js"></script>
+    <script src="https://source.zoom.us/3.11.0/lib/vendor/redux-thunk.min.js"></script>
+    <script src="https://source.zoom.us/3.11.0/lib/vendor/lodash.min.js"></script>
+    <script src="https://source.zoom.us/zoom-meeting-3.11.0.min.js"></script>
+    <script src="https://source.zoom.us/3.11.0/zoom-meeting-embedded-3.11.0.min.js"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             // Ensure ZoomMtg is defined
             if (typeof ZoomMtg !== 'undefined') {
-                ZoomMtg.setZoomJSLib('https://source.zoom.us/2.9.5/lib', '/av');
+                ZoomMtg.setZoomJSLib('https://source.zoom.us/3.11.0/lib', '/av');
                 ZoomMtg.preLoadWasm();
                 ZoomMtg.prepareWebSDK();
                 ZoomMtg.i18n.load('en-US');
@@ -43,6 +43,8 @@
                     leaveUrl: 'http://localhost:8000',
                     role: 0
                 };
+
+                console.log(meetConfig);
 
                 ZoomMtg.init({
                     leaveUrl: meetConfig.leaveUrl,
